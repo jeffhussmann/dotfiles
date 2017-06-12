416e03c409e44cba3b4aaf0eb60244ff61072086
Jeff Hussmann <jeff.hussmann@gmail.com>
2017-06-12 14:34:09 -0700

    adds gtkrc-2.0 to symlinks

--- make_sym_links.sh
+++ make_sym_links.sh
@@ -3,7 +3,7 @@
 
 dir=~/dotfiles
 olddir=~/dotfiles_old
-files="bashrc vimrc gvimrc inputrc gitconfig"
+files="bashrc vimrc gvimrc inputrc gitconfig gtkrc-2.0"
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
